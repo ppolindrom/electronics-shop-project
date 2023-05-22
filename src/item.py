e5bc735fdce3818f040f8e92751d3cde92ce5cc9
ppolindrom <ipollyce@mail.ru>
2023-05-22 15:41:09 +0300

    add new change

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+import os
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,23 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        try:
+            if len(name) <= 10:
+                self.__name = name
+        except:
+            print("Наименование товара превышает 10 символов")
+
 
     def calculate_total_price(self) -> float:
         """
@@ -30,5 +46,19 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price *= (1 - self.pay_rate / 100)
+        self.price *= self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        cls.all.clear()
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(dir_path + '/items.csv', encoding='utf-8') as csv_file:
+            reader = csv.DictReader(csv_file)
+
+            for row in reader:
+                cls(row['name'], row['price'], row['quantity'])
+
+    @staticmethod
+    def string_to_number(num):
+        numb = float(num)
+        return int(numb)
